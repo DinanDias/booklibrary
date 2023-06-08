88bd2d4bc619873502af14a131d548165054731a
DinanDias <93309200+DinanDias@users.noreply.github.com>
2023-06-08 23:00:43 +0530

    created onget handler in index.cshtml.cs

--- booklibrary/Views/BookList/Index.cshtml.cs
+++ booklibrary/Views/BookList/Index.cshtml.cs
@@ -1,12 +1,28 @@
+using booklibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace booklibrary.Views.BookList
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public IEnumerable Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
         }
     }
 }
