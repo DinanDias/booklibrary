81b4fece1c89758bf72d08e99c669a1a230ec0bc
DinanDias <93309200+DinanDias@users.noreply.github.com>
2023-06-09 23:27:41 +0530

    Fixed BookIndex View

---
+++ booklibrary/Controllers/BookListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace booklibrary.Controllers
+{
+	public class BookListController : Controller
+	{
+		public IActionResult BookIndex()
+		{
+			return View();
+		}
+	}
+}
